7f4de144a50bf40ea7cb210b38254b434c5814fb
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-09-02 14:15:21 +0300

    Update useFetsh.jsx

--- client/src/components/hooks/useFetsh.jsx
+++ client/src/components/hooks/useFetsh.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 const fetch_data =(method,subUrl,data)=>{
- return axios[method](`${process.env.REACT_APP_BACKEND_URL}${subUrl}`,data)
+ return axios[method](`https://dashbord-1-0-0.onrender.com/${subUrl}`,data)
 }
 export const useFetsh = (method,subUrl,action,data=null)=>{
  const onSuccess =(data =>{
