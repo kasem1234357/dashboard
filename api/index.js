57035433eec4c438b5cfb8a78dbada22d8e00c9b
kasem1234357 <kasemalolo2002@gmail.com>
2023-10-08 21:36:25 +0300

    fix implement authintication

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const userRoute = require("./routes/user");
 const app = express();
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin','https://dashboard-magic.vercel.app')
+  res.header('Access-Control-Allow-Methods','POST, GET, OPTIONS, PUT, DELETE')
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
