ef30a6bc0b6a2c7c559cf55e01d9ea13fbffb497
kasem1234357 <kasemalolo2002@gmail.com>
2024-01-20 23:30:35 +0200

    cloudinary logic update v1

--- api/index.js
+++ api/index.js
@@ -14,13 +14,11 @@ const userRoute = require("./routes/user");
 const app = express();
 let allowlist = ['https://dashboard-magic.vercel.app/', 'https://dashboard-magic.vercel.app']
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods','POST, GET, OPTIONS, PUT, DELETE')
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
+  app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 dotenv.config();
