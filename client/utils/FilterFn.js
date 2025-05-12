32b795791e6eda17572693318848dea9fea5e282
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2025-05-13 02:11:04 +0300

    Delete client/utils directory

--- client/utils/FilterFn.js
+++
@@ -1,7 +0,0 @@
-export const filter = (data,method, filterText,callback) => {
-    callback((prev)=>{
-      const dt = data.filter((task) => task[method].includes(filterText));
-      prev = dt
-      return prev
-    });
-  };
